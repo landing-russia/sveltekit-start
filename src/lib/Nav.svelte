2cfc2e934a635bd17b1529a442f3303e69d9ffed
landing-russia <landing-russia@yandex.ru>
2022-02-17 12:10:33 +0300

    dark mode setup

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -83,7 +83,7 @@
         </svg></button
       >
     </div>
-    <div class="flex space-x-3">
+    <div class="flex items-center space-x-3">
       <a
         href="/"
         class="py-4 px-2 text-lg font-medium text-gray-500 hover:text-gray-800 transition duration-200"
@@ -94,6 +94,47 @@
         class="py-4 px-2 text-lg font-medium text-gray-500 hover:text-gray-800 transition duration-200"
         >About</a
       >
+      {#if isDark === "dark"}
+        <button
+          type="button"
+          class="ml-4 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-amber-300"
+          on:click={toggleDarkMode}
+          ><svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6 text-amber-500 hover:text-amber-700"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+            />
+          </svg></button
+        >
+      {:else if isDark === "light"}
+        <button
+          type="button"
+          class="ml-4 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-amber-300"
+          on:click={toggleDarkMode}
+          ><svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6 text-amber-500 hover:text-amber-700"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+            />
+          </svg></button
+        >
+      {/if}
     </div>
   </nav>
 </div>
