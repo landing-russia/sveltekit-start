0d9bdd3c627a5340dfbb5e9dc3c9b8730928b25b
landing-russia <landing-russia@yandex.ru>
2022-05-19 14:48:40 +0300

    update tailwind.config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,26 +4,39 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 const config = {
   // mode: "jit",
   content: ["./src/**/*.{html,js,svelte,ts}"],
-  darkMode: 'class', // or 'media' or 'class'
+  darkMode: "class", // or 'media' or 'class'
 
   theme: {
     screens: {
-      xs: "540px",
+      xs: "340px",
       ...defaultTheme.screens,
+      "3xl": "1900px",
     },
     extend: {
-      // colors,
+      colors: {
+        "dark-rose": "#62011F",
+      },
       fontFamily: {
-        inter: "Inter, sans-serif",
+        bitter: "Bitter, serif",
+        open: "Open Sans, sans-serif",
       },
       fontSize: {
-        '10xl': '10rem'
+        "7.5xl": "5.25rem",
+        "9.5xl": "9rem",
+        "10xl": "10rem",
       },
       maxWidth: {
         "1/4": "25%",
         "1/2": "50%",
         "3/4": "75%",
         90: "90%",
+      },
+      spacing: {
+        13: "3.25rem",
+        15: "3.75rem",
+        67: "16.75rem",
+        128: "32rem",
+        144: "36rem",
       },
     },
   },
